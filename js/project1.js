2c0ec96dab0e7a8c5aaa0842584e16dbf1a4c7f3
dvnilsen <dvnilsen@protonmail.com>
2022-12-03 12:54:20 -0800

    sixth commit

--- js/project1.js
+++ js/project1.js
@@ -3,19 +3,33 @@ let spaceZero = document.getElementById("0");
 
 spaceZero.addEventListener("click", function() {
     console.log(spaceZero.id); 
-    spaceZero.style.backgroundColor = "red";
+    spaceZero.style.backgroundColor = "darkred";
 })
 
 //create the 42 game spaces
 for(let i=1; i<42; i++) {
     let newSpace = document.createElement("div");
     newSpace.classList.add("space");
+    //adds a unique ID number to each div in the 'space' class
     newSpace.setAttribute("id", i); 
     //After creating them, add them to the screen
     board.append(newSpace);
     newSpace.addEventListener("click", function() {
-        console.log(newSpace.id); 
-        newSpace.style.backgroundColor = "red";
-    })
+        //console.log(newSpace.id); 
+        let newSpaceID = newSpace.id;
+        let numberID = Number(newSpaceID);
+        //console.log(numberID + 7); 
+        let newSpaceNumber = numberID; 
+        
+        
+        newSpace.style.backgroundColor = "darkred";
+    
+    }) 
+}
+
+// function to call when spaces are clicked
+
+function dropPiece() {
+
 }
 
